adfb1a7ce7e341fd9e98851e58b2567cff42aca2
Hao Jiang <joshtrick@outlook.com>
2025-03-18 22:43:17 +0800

    update

--- xerrors/errors.go
+++ xerrors/errors.go
@@ -11,6 +11,14 @@ func (e *Error) Error() string {
 	return fmt.Sprintf("code: %d, msg: %s", e.Code, e.Msg)
 }
 
+// Detail is a short hand function to create a New error by adding err.Error() to e.Msg
+func (e *Error) Detail(err error) error {
+	return New(
+		e.Code,
+		fmt.Sprintf("%s: %s", e.Msg, err.Error()),
+	)
+}
+
 func New(code int, msg string) error {
 	return &Error{
 		Code: code,
